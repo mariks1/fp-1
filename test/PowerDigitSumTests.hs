2111e2f6d9f5621b8c9336570fc3ca1b00db5013
mariks1 <marat-khasanshin@list.ru>
2024-10-05 01:21:30 +0300

    fix

--- test/PowerDigitSumTests.hs
+++ test/PowerDigitSumTests.hs
@@ -9,5 +9,9 @@ tests =
   testGroup
     "16 problem"
     [ testCase "Recursion Method Test" $
-        recursionMethod 2 ^ 1000 @?= 1366
+        recursionMethod (2 ^ 1000) @?= 1366,
+      testCase "Tail Recursion Method Test" $
+        tailRecursionMethod (2 ^ 1000) @?= 1366,
+      testCase "Map Method Test" $
+        mapMethod (2 ^ 1000) @?= 1366
     ]
---
+++ test/Spec.hs
@@ -0,0 +1,12 @@
+import qualified PowerDigitSumTests
+import Test.Tasty
+
+main :: IO ()
+main = defaultMain tests
+
+tests :: TestTree
+tests =
+  testGroup
+    "All tests"
+    [ PowerDigitSumTests.tests
+    ]
